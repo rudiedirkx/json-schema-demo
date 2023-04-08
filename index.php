3ec098104d8f7a2d3376643751867841b12826a8
Rudie Dirkx <github@hotblocks.nl>
2023-04-08 23:54:40 +0200

    nette/schema

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 
+use Nette\Schema\Expect;
+
 require 'vendor/autoload.php';
+
+header('Content-type: text/plain; charset=utf-8');
+
+$data = [
+	'a11y' => true,
+	'nope' => 123,
+];
+
+
 
 $schema = [
 	'type' => 'object',
@@ -12,12 +23,24 @@ $schema = [
 	],
 ];
 
-$data = [
-	'a11y' => true,
-	'nope' => 123,
-];
-
 $validator = new JsonSchema\Validator;
 $validator->validate($data, $schema, JsonSchema\Constraints\Constraint::CHECK_MODE_TYPE_CAST);
 var_dump($validator->isValid());
 print_r($validator->getErrors());
+
+
+echo "\n\n";
+
+
+$schema = Expect::structure([
+	'a11y' => Expect::bool(),
+]);
+
+try {
+	$processor = new Nette\Schema\Processor;
+	$normalized = $processor->process($schema, $data);
+	print_r($normalized);
+}
+catch (Nette\Schema\ValidationException $ex) {
+	echo 'Data is invalid: ' . $ex->getMessage() . "\n";
+}
