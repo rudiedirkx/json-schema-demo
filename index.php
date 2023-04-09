a6c94bd079594c78143cbb9a2268dda6653e3493
Rudie Dirkx <github@hotblocks.nl>
2023-04-09 04:10:43 +0200

    custom nette validation

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ header('Content-type: text/plain; charset=utf-8');
 
 $data = [
 	'a11y' => true,
-	'nope' => 123,
+	// 'nope' => 123,
+	'start' => '2023-01-01',
 ];
 
 
@@ -20,6 +21,11 @@ $schema = [
 			'type' => 'boolean',
 			'default' => false,
 		],
+		'start' => [
+			'type' => 'string',
+			'format' => 'date',
+			// 'default' => false,
+		],
 	],
 ];
 
@@ -29,17 +35,72 @@ var_dump($validator->isValid());
 print_r($validator->getErrors());
 
 
-echo "\n\n";
 
+echo "\n\n\n";
+
+
+
+class CustomValidationError extends Exception {
+	public function __construct(
+		string $message,
+		protected array $params = [],
+		protected ?string $messageType = null,
+	) {
+		parent::__construct($message);
+	}
+
+	public function getParams() : array {
+		return $this->params;
+	}
+
+	public function getMessageType() : string {
+		return $this->messageType ?? Nette\Schema\Message::TYPE_MISMATCH;
+	}
+}
+
+abstract class CustomType implements Nette\Schema\Schema {
+	use Nette\Schema\Elements\Base;
+
+	function normalize($value, Nette\Schema\Context $context) {
+// var_dump(__LINE__);
+		try {
+			$this->validateValue($value);
+		}
+		catch (CustomValidationError $ex) {
+			$context->addError($ex->getMessage(), $ex->getMessageType(), $ex->getParams());
+		}
+
+		return $this->doNormalize($value, $context);
+	}
+
+	function merge($value, $base) {}
+
+	function complete($value, Nette\Schema\Context $context) {
+		return $this->doFinalize($value, $context);
+	}
+}
+
+class CustomDate extends CustomType {
+	protected function validateValue(mixed &$value) : void {
+		if (!is_string($value) || !preg_match('#^\d\d\d\d-\d\d-\d\d$#', $value)) {
+			throw new CustomValidationError(
+				'The %label% %path% must be a date string, %value% given.',
+				['value' => is_scalar($value) ? $value : gettype($value)],
+			);
+		}
+	}
+}
 
 $schema = Expect::structure([
 	'a11y' => Expect::bool(),
+	'start' => new CustomDate(),
 ]);
 
 try {
 	$processor = new Nette\Schema\Processor;
-	$normalized = $processor->process($schema, $data);
-	print_r($normalized);
+	$normalized = $processor->process($schema->castTo('array'), $data);
+	var_dump(true);
+	// print_r($normalized);
 }
 catch (Nette\Schema\ValidationException $ex) {
 	echo 'Data is invalid: ' . $ex->getMessage() . "\n";
